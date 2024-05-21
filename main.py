0c06978f30077188440d790508109bdb83428442
TheodoreEpitech <theodore.magna@epitech.eu>
2024-05-21 23:01:33 +0200

    add: working pokedex and savefile

--- main.py
+++ main.py
@@ -3,13 +3,15 @@
 #-------------------------
 import subprocess
 from random import randint
+import json
 
 #-------------------------
 # Config
 #-------------------------
 shiny_rate = 1  # (in %)
-catch_rate = 50 # (in %)
+catch_rate = 1 # (multiplier)
 team_size = 6   # (in nb of pokemons)
+max_lvl = 100   # (in lvl)
 
 #-------------------------
 # Check if Krabby is installed
@@ -31,12 +33,19 @@ class Pokemon():
         self.name = name
         self.shiny = shiny
         self.level = level
+        self.id = pokemons.index(name) + 1
     
     def __str__(self):
         return self.name
+    
+    def spawn(self):
+        if (randint(1, 100) > 100 - shiny_rate):
+            self.shiny = True
+
+        self.level = randint(1, max_lvl)
 
     def catch(self, team):
-        if (randint(1, 100) >= catch_rate):
+        if (randint(1, max_lvl) >= self.level / catch_rate):
             print("You caught a " + self.name + " !")
             team.append(self)
         else:
@@ -48,11 +57,20 @@ class Pokemon():
             team.remove(self)
 
     def show(self):
-        args_list = ['krabby', 'name', self.name]
+        print("ID: " + str(self.id))
+        print("Shiny: " + str(self.shiny))
+        print("LVL: " + str(self.level))
+        args_list = ['krabby', 'name', self.name, '-i']
         if (self.shiny):
             args_list.append("-s")
-            print("Shiny !")
         subprocess.run(args_list)
+
+    def dump(self):
+        return {
+            "name": self.name,
+            "shiny": self.shiny,
+            "level": self.level
+        }
 
 #-------------------------
 # Tools
@@ -71,13 +89,59 @@ def show_team(team):
     print("=============================================================")
     input("Press Enter to continue...")
 
+def show_pokedex(pokedex):
+    clear_terminal()
+    print("=============================================================")
+    print("=============================================================")
+    print("Your pokedex:")
+    for poke_id, poke in pokedex.items():
+        print(str(poke_id) + " - " + poke["name"] + " - Level: " + str(poke["level"]) + " - Shiny: " + str(poke["shiny"]))
+    print("=============================================================")
+    print("=============================================================")
+    temp = input("List of actions:\n- 1: Continue\n- 2: Show Pokemon\n")
+    match temp:
+        case ("1"):
+            return
+        case ("2"):
+            temp = input("Enter the ID of the pokemon you want to see: ")
+            if (temp in pokedex):
+                pokemon = Pokemon(pokedex[temp]["name"], pokedex[temp]["shiny"], pokedex[temp]["level"])
+                pokemon.show()
+            else:
+                print("Pokemon not found")
+            input("Press Enter to continue...")
+            show_pokedex(pokedex)
+        case _:
+            print("Invalid action")
+            show_pokedex(pokedex)
+
+def save_team_in_pokedex(team, pokedex):
+    for poke in team:
+
+        pokemon_id = str(poke.id)
+        for _ in range(len(pokemon_id), 3):
+            pokemon_id = "0" + pokemon_id
+
+        if (pokemon_id not in pokedex):
+            pokedex[pokemon_id] = poke.dump()
+        elif (pokedex[pokemon_id]["level"] < poke.level and not pokedex[pokemon_id]["shiny"]):
+            pokedex[pokemon_id] = poke.dump()
+        elif (not pokedex[pokemon_id]["shiny"] and poke.shiny):
+            pokedex[pokemon_id] = poke.dump()
+        elif (pokedex[pokemon_id]["shiny"] and poke.shiny and pokedex[pokemon_id]["level"] < poke.level):
+            pokedex[pokemon_id] = poke.dump()
+    
+    pokedex = dict(sorted(pokedex.items()))
+    with open("pokedex.json", "w") as file:
+        json.dump(pokedex, file)
+
 #-------------------------
 # Game Loop
 #-------------------------
-def get_action(pokemon, team):
+def get_action(pokemon, team, pokedex):
     clear_terminal()
     pokemon.show()
-    temp = input("List of actions:\n- 1: Catch\n- 2: Continue\n- 3: Show Team\n- 4: Exit\n")
+    temp = input("List of actions:\n- 1: Catch\n- 2: Continue\n- 3: Show Team\n- 4: Show Pokedex\n- 5: Exit\n")
     match temp:
         case ("1"):
             pokemon.catch(team)
@@ -87,28 +151,35 @@ def get_action(pokemon, team):
         case ("3"):
             show_team(team)
         case ("4"):
+            show_pokedex(pokedex)
+        case ("5"):
             exit()
         case _:
             print("Invalid action")
-    get_action(pokemon, team)
+    get_action(pokemon, team, pokedex)
 
 #-------------------------
 # Main
 #-------------------------
 def main():
     team = []
+    try:
+        pokedex = json.load(open("pokedex.json"))
+    except:
+        pokedex = {}
 
     while (len(team) < team_size):
         nb = randint(0, len(pokemons) - 1)
         pokemon = Pokemon(pokemons[nb])
 
-        if (randint(1, 100) > 100 - shiny_rate):
-            pokemon.shiny = True
+        pokemon.spawn()
 
-        get_action(pokemon, team)
+        get_action(pokemon, team, pokedex)
 
-    show_team()
+    show_team(team)
     print("You have a full team !")
+    save_team_in_pokedex(team, pokedex)
+    print("The team got added to the pokedex !")
 
 if __name__ == "__main__":
     main()
